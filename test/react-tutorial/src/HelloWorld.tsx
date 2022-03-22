0e69df86227d6dfd43008933849145e4f3759c0b
laft <laft@kmc.gr.jp>
2022-03-23 03:50:26 +0900

    要らないものを色々消す

--- test/react-tutorial/src/HelloWorld.tsx
+++ test/react-tutorial/src/HelloWorld.tsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const HelloWorld: React.FC = () => <h1>Hello <strong>this</strong> World!!</h1>;
-
-export default HelloWorld;
-
-
 function calculateWinner(squares: SquareState[]) {
   const lines = [
     [0, 1, 2],
@@ -26,21 +21,6 @@ function calculateWinner(squares: SquareState[]) {
   return null;
 }
 
-
-export class ShoppingList extends React.Component<{ name: string }> {
-  render() {
-    return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
-    );
-  }
-}
 
 type SquareState = 'O' | 'X' | null
 type SquareProps = {
--- test/react-tutorial/src/app.tsx
+++ test/react-tutorial/src/app.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import HelloWorld from './HelloWorld';
-import { ShoppingList, Game } from './HelloWorld'
+import { Game } from './HelloWorld'
 
 ReactDom.render(
   <React.StrictMode>
-    <HelloWorld />
-    <ShoppingList name='sugar' />
     <Game />
   </React.StrictMode>,
   document.getElementById('app')
